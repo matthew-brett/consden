a76d11761a8b98228439751a512a158545025f5c
Matthew Brett <matthew.brett@gmail.com>
2015-12-23 11:22:20 +0000

    RF: move consden code into own module

---
+++ consden/spm_funcs.py
@@ -0,0 +1,88 @@
+""" This module defines SPM-like functions
+
+Run the tests with::
+
+    nosetests test_spm_funcs.py
+
+The functions have docstrings according to the numpy docstring standard - see:
+
+    https://github.com/numpy/numpy/blob/master/doc/HOWTO_DOCUMENT.rst.txt
+"""
+# Python 3 compatibility
+from __future__ import print_function, division
+
+import numpy as np
+from scipy.stats import gamma
+
+import nibabel as nib
+
+
+def spm_global(vol):
+    """ Calculate SPM global metric for array `vol`
+
+    Parameters
+    ----------
+    vol : array
+        Array giving image data, usually 3D.
+
+    Returns
+    -------
+    g : float
+        SPM global metric for `vol`
+    """
+    T = np.mean(vol) / 8
+    return np.mean(vol[vol > T])
+
+
+def get_spm_globals(fname):
+    """ Calculate SPM global metrics for volumes in image filename `fname`
+
+    Parameters
+    ----------
+    fname : str
+        Filename of file containing 4D image
+
+    Returns
+    -------
+    spm_vals : array
+        SPM global metric for each 3D volume in the 4D image.
+    """
+    img = nib.load(fname)
+    data = img.get_data()
+    spm_vals = []
+    for i in range(data.shape[-1]):
+        vol = data[..., i]
+        spm_vals.append(spm_global(vol))
+    return spm_vals
+
+
+def spm_hrf(times):
+    """ Return values for standard SPM HRF at given `times`
+
+    This is the same as SPM's ``spm_hrf.m`` function using the default input
+    values.
+
+    Parameters
+    ----------
+    times : array
+        Times at which to sample hemodynamic response function
+
+    Returns
+    -------
+    values : array
+        Array of same length as `times` giving HRF samples at corresponding
+        time post onset (where onset is T==0).
+    """
+    # Gamma only defined for x values > 0
+    time_gt_0 = times > 0
+    ok_times = times[time_gt_0]
+    # Output vector
+    values = np.zeros(len(times))
+    # Gamma pdf for the peak
+    peak_values = gamma.pdf(ok_times, 6)
+    # Gamma pdf for the undershoot
+    undershoot_values = gamma.pdf(ok_times, 16)
+    # Combine them
+    values[time_gt_0] = peak_values - undershoot_values / 6.
+    # Divide by sum
+    return values / np.sum(values)
---
+++ consden/tests/test_stimuli.py
@@ -0,0 +1,40 @@
+""" Test stimuli module
+
+Run tests with::
+
+    nosetests test_stimuli.py
+"""
+
+from os.path import dirname, join as pjoin
+
+import numpy as np
+import numpy.testing as npt
+
+from .. import stimuli
+
+HERE = dirname(__file__)
+
+
+def test_events2neural():
+    # test events2neural function
+    cond_path = pjoin(HERE, 'cond_test1.txt')
+    neural = stimuli.events2neural(cond_path, 2, 16)
+    # cond_test1.txt file is:
+    """
+    10    5.0    1
+    20    4.0    2
+    24    3.0    0.1
+    """
+    # Expected values for tr=2, n_trs=16
+    expected = np.zeros(16)
+    expected[5:7] = 1
+    expected[10:12] = 2
+    expected[12] = 0.1
+    npt.assert_array_equal(neural, expected)
+    neural = stimuli.events2neural(cond_path, 1, 30)
+    # Expected values for tr=1, n_trs=30
+    expected = np.zeros(30)
+    expected[10:15] = 1
+    expected[20:24] = 2
+    expected[24:27] = 0.1
+    npt.assert_array_equal(neural, expected)
