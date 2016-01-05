78fd6571e24d29b10e38c7eda8804af87bb371ff
Matthew Brett <matthew.brett@gmail.com>
2016-01-05 23:21:56 +0000

    WIP: continuing buildup of openfmri objects

--- consden/openfmri.py
+++ consden/openfmri.py
@@ -4,8 +4,13 @@ import os
 from os.path import join as pjoin, abspath
 import re
 
-RE_SUB_NO = re.compile("sub(\d+)$")
-RE_TASK_RUN_NO = re.compile("task(\d+)_run(\d+)$")
+import numpy as np
+
+RE_SUB_NO = re.compile(r"sub(\d+)$")
+RE_TASK_RUN_NO = re.compile(r"task(\d+)_run(\d+)$")
+RE_MODEL_NO = re.compile(r"model(\d+)$")
+RE_COND_NO = re.compile(r"cond(\d+).txt$")
+
 
 def path_to_subj(path):
     match = RE_SUB_NO.search(path)
@@ -39,44 +44,120 @@ def get_tasks_runs(task_run_path):
     return tasks_runs
 
 
+def get_model_nos(models_path):
+    models = []
+    for fname in os.listdir(models_path):
+        model_match = RE_MODEL_NO.match(fname)
+        if model_match is None:
+            continue
+        models.append(int(model_match.groups()[0]))
+    return tuple(models)
+
+
+def get_conditions(model_run_path):
+    conditions = []
+    for fname in os.listdir(model_run_path):
+        cond_match = RE_COND_NO.match(fname)
+        if cond_match is None:
+            continue
+        ons_dur_amp = np.loadtxt(pjoin(model_run_path, fname))
+        conditions.append((int(cond_match.groups()[0]), ons_dur_amp))
+    return conditions
+
+
+class Run(object):
+
+    def __init__(self, subject, task_no, run_no):
+        self.subject = subject
+        self.task_no = task_no
+        self.run_no = run_no
+
+    @property
+    def path(self):
+        return pjoin(self.subject.path, 'BOLD',
+                     'task{0:03d}_run{1:03d}'.format(
+                         self.task_no, self.run_no))
+
+    def get_bold_fname(self, prefix=''):
+        return pjoin(self.path, prefix + 'bold.nii.gz')
+
+
+
+class RunModel(object):
+
+    def __init__(self, run, models):
+        self.run = run
+        self.models = models
+
+    @property
+    def model_no(self):
+        return self.models.model_no
+
+    @property
+    def task_no(self):
+        return self.run.task_no
+
+    @property
+    def run_no(self):
+        return self.run.run_no
+
+    @property
+    def path(self):
+        return pjoin(self.models.path,
+                     'onsets',
+                     'task{0:03d}_run{1:03d}'.format(
+                         self.task_no, self.run_no))
+
+    @property
+    def conditions(self):
+        return get_conditions(self.path)
+
+
+class Models(object):
+    run_model_maker = RunModel
+
+    def __init__(self, subject, model_no, runs):
+        self.subject = subject
+        self.model_no = model_no
+        self.runs = runs
+
+    @property
+    def path(self):
+        return pjoin(self.subject.path,
+                     'model',
+                     'model{0:03d}'.format(self.model_no))
+
+    @property
+    def run_models(self):
+        rms = []
+        for run in self.runs:
+            rms.append(self.run_model_maker(run, self))
+        return rms
+
+
 class Subject(object):
+    run_maker = Run
+    models_maker = Models
 
     def __init__(self, root_path, subj_no):
         self.root_path = abspath(root_path)
         self.subj_no = subj_no
-        self.path = pjoin(self.root_path, 'sub{0:03d}'.format(subj_no))
-        self.bold_path = pjoin(self.path, 'BOLD')
 
     @property
-    def tasks(self):
-        return []
+    def path(self):
+        return pjoin(self.root_path, 'sub{0:03d}'.format(self.subj_no))
 
-"""
-A Subject has
+    @property
+    def runs(self):
+        runs = []
+        for task_no, run_no in get_tasks_runs(pjoin(self.path, 'BOLD')):
+            runs.append(self.run_maker(self, task_no, run_no))
+        return runs
 
-* None or more Runs
-
-A Run has a:
-
-* task number
-* run number
-* None or more models
-
-A Model has a:
-
-* model number
-* task number
-* run number
-* None or more onsets
-"""
-
-class Run(object):
-    # task
-    # models
-    pass
-
-
-class Task(object):
-    # runs
-    # models
-    pass
+    @property
+    def models(self):
+        models = []
+        runs = self.runs
+        for model_no in get_model_nos(pjoin(self.path, 'model')):
+            models.append(self.models_maker(self, model_no, runs))
+        return models
--- consden/tests/test_openfmri.py
+++ consden/tests/test_openfmri.py
@@ -1,13 +1,13 @@
 """ Testing openfmri module
 """
 
-from os.path import dirname, join as pjoin, isdir
+from os.path import dirname, join as pjoin, isdir, isfile
 from itertools import product
 
 import numpy as np
 
 from ..openfmri import (path_to_subj, path_to_task_run, get_subjects,
-                        get_tasks_runs)
+                        get_tasks_runs, get_model_nos, get_conditions)
 
 from numpy.testing import (assert_almost_equal,
                            assert_array_equal)
@@ -46,3 +46,27 @@ def test_get_tasks_runs():
                  pjoin(SUB1, 'model', 'model002', 'onsets')):
         tasks_runs = get_tasks_runs(path)
         assert_equal(tasks_runs, list(product(range(1, 6), range(1, 3))))
+
+
+def test_get_model_nos():
+    assert_equal(get_model_nos(pjoin(SUB1, 'model')), (1, 2))
+
+
+def test_runs():
+    subject = get_subjects(DS114)[0]
+    for run in subject.runs:
+        assert_true(isfile(run.get_bold_fname()))
+
+
+def test_models():
+    subject = get_subjects(DS114)[0]
+    models = subject.models
+    assert_equal(len(models), 2)
+    # Number of conditions per task, for both models
+    exp_lens = {1: 1, 2: 1, 3: 3, 4:1, 5:5}
+    for model in models:
+        for model_run in model.run_models:
+            assert_true(isdir(model_run.path))
+            conds = model_run.conditions
+            assert_equal(len(conds), exp_lens[model_run.task_no])
+            assert_true(isfile(model_run.run.get_bold_fname()))
