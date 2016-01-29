9f10d9cbe70ff3374aa879183cf25d0db549ab1d
Matthew Brett <matthew.brett@gmail.com>
2016-01-29 13:03:49 -0800

    Update subtraction script for func_prefix

--- consden_subtract.py
+++ consden_subtract.py
@@ -8,17 +8,24 @@ import numpy as np
 import nibabel as nib
 
 root_path = expanduser('~/data/ds114')
+func_prefix = 'waf'
+
+def ffn(fname):
+    path, fpart = psplit(fname)
+    return pjoin(path, func_prefix + fpart)
+
 
 for dirpath, dirnames, filenames in os.walk(root_path):
-    if not 'b_n.nii' in filenames:
+    b_n_fpart = ffn('b_n.nii')
+    if not b_n_fpart in filenames:
         continue
-    n_img = nib.load(pjoin(dirpath, 'b_n.nii'))
-    e_img = nib.load(pjoin(dirpath, 'b_e.nii'))
+    n_img = nib.load(pjoin(dirpath, b_n_fpart))
+    e_img = nib.load(pjoin(dirpath, ffn('b_e.nii')))
     diff_vol = n_img.get_data()[..., 0] - e_img.get_data()[..., 0]
     nib.save(nib.Nifti1Image(diff_vol, n_img.affine, n_img.header),
-             pjoin(dirpath, 'task_diff.nii'))
+             pjoin(dirpath, ffn('task_diff.nii')))
     mask = diff_vol == 0
-    c_img = nib.load(pjoin(dirpath, 'b_c.nii'))
+    c_img = nib.load(pjoin(dirpath, ffn('b_c.nii')))
     coeffs = c_img.get_data()[mask].T.astype(float)
-    nz = np.sum(coeffs, axis=0)
+    nz = np.sum(coeffs, axis=1)
     print(dirpath, nz, mask.shape, c_img.shape)
