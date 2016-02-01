1187b4bca7e73c993121fec0e2cc9cd754c38344
Matthew Brett <matthew.brett@gmail.com>
2016-02-01 12:36:25 -0800

    Try multiprocessing

--- consden_ds114.py
+++ consden_ds114.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 
 from os.path import expanduser, split as psplit, join as pjoin, exists
+import multiprocessing
 
 import numpy as np
 
@@ -31,30 +32,44 @@ def or_gz(fname):
             return shorter
     return fname
 
-for subject in subjects:
-    model = subject.models[0]
-    for run_model in model.run_models:
-        if run_model.task_no != 2:
-            continue
-        func_fname = run_model.run.get_bold_fname()
-        func_fname = or_gz(prefix_fname(func_fname, func_prefix))
-        print('Processing', func_fname)
-        cond_no, ons_dur_amp = run_model.conditions[0]
-        block_spec = openfmri2nipy(ons_dur_amp)
-        contrasts, B_n, B_e, B_c, mask  = analyze_4d([('motor', block_spec)],
-                                                     func_fname,
-                                                     t1_constant,
-                                                     TR = TR,
-                                                     n_dummies=n_dummies,
-                                                     dct_order=dct_order)
-        out_path, _ = psplit(func_fname)
-        nib.save(B_n, pjoin(out_path, func_prefix + 'b_n.nii'))
-        nib.save(B_e, pjoin(out_path, func_prefix + 'b_e.nii'))
-        nib.save(B_c, pjoin(out_path, func_prefix + 'b_c.nii'))
-        nib.save(mask, pjoin(out_path, func_prefix + 'mask.nii'))
-        task_diff = B_n.get_data()[..., 0] - B_e.get_data()[..., 0]
-        task_diff = nib.Nifti1Image(task_diff, B_n.affine, B_n.header)
-        nib.save(task_diff, pjoin(out_path, func_prefix + 'task_diff.nii'))
-        msk = mask.get_data().astype(bool)
-        coefs = B_c.get_data()[msk].T
-        print(np.where(np.sum(coefs, axis=1))[0])
+
+def analyze_model(run_model):
+    func_fname = run_model.run.get_bold_fname()
+    func_fname = or_gz(prefix_fname(func_fname, func_prefix))
+    print('Processing', func_fname)
+    cond_no, ons_dur_amp = run_model.conditions[0]
+    block_spec = openfmri2nipy(ons_dur_amp)
+    contrasts, B_n, B_e, B_c, mask  = analyze_4d([('motor', block_spec)],
+                                                    func_fname,
+                                                    t1_constant,
+                                                    TR = TR,
+                                                    n_dummies=n_dummies,
+                                                    dct_order=dct_order)
+    out_path, _ = psplit(func_fname)
+    nib.save(B_n, pjoin(out_path, func_prefix + 'b_n.nii'))
+    nib.save(B_e, pjoin(out_path, func_prefix + 'b_e.nii'))
+    nib.save(B_c, pjoin(out_path, func_prefix + 'b_c.nii'))
+    nib.save(mask, pjoin(out_path, func_prefix + 'mask.nii'))
+    task_diff = B_n.get_data()[..., 0] - B_e.get_data()[..., 0]
+    task_diff = nib.Nifti1Image(task_diff, B_n.affine, B_n.header)
+    nib.save(task_diff, pjoin(out_path, func_prefix + 'task_diff.nii'))
+    msk = mask.get_data().astype(bool)
+    coefs = B_c.get_data()[msk].T
+    print(np.where(np.sum(coefs, axis=1))[0])
+
+
+def gen_models(subjects):
+    for subject in subjects:
+        model = subject.models[0]
+        for run_model in model.run_models:
+            if run_model.task_no != 2:
+                continue
+            yield run_model
+
+
+if __name__ == '__main__':
+    jobs = []
+    for model in gen_models(subjects):
+        p = multiprocessing.Process(target=analyze_model, args=(model,))
+        jobs.append(p)
+        p.start()
